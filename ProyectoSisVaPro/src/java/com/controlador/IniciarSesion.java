ae6032badee2b8f0a6f5a38c8f1715e7dd2a18b3
dsarm <dsarm@192.168.1.22>
2017-10-11 00:27:07 -0600

    Ya funciona el Loguin

--- ProyectoSisVaPro/src/java/com/controlador/IniciarSesion.java
+++ ProyectoSisVaPro/src/java/com/controlador/IniciarSesion.java
@@ -6,8 +6,8 @@ import javax.servlet.http.*;
 import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
+import javax.servlet.ServletException;import javax.servlet.http.HttpServlet;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
