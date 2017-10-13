5e285655c0f08dc1fd253cc6702309b2dd6bd762
dsarm <dsarm@192.168.1.9>
2017-10-12 21:40:19 -0600

    Creacion del proyecto, Eliminando index.html, y agregando extructura MVC

---
+++ com.sisvapro/src/java/com/conexion/Conexion.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * Nombre de la Clase: Conexion
+ * Fecha: 12 de Octubre de 2017
+ * CopyRight: SISVAPRO SA. De CV.
+ * @author David Sarmiento
+ */
+public class Conexion {
+    private Connection con;
+
+    public Connection getCon() {
+        return con;
+    }
+
+    public void setCon(Connection con) {
+        this.con = con;
+    }
+
+    public void conectar() throws Exception{
+        try {
+           Class.forName("org.postgresql.Driver");
+           con=DriverManager.getConnection(
+                   "jdbc:postgresql://localhost:5432/Sisvapro?user=postgres&password=root"); 
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+    public void desconectar() throws Exception{
+        try {
+            if (con!=null) {
+                if (con.isClosed()==false) {
+                    con.close();
+                }
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}
---
+++ com.sisvapro/web/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index.jsp
+    Created on : Oct 12, 2017, 9:28:42 PM
+    Author     : David Sarmiento
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Index</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
