c31bbe0854dec9d05559efc4431735265e33938d
carlos <carlos@DESKTOP-TFRKBLK>
2017-10-15 22:52:38 -0600

    agregando crud . . . :Vc

---
+++ com.sisvapro/src/java/com/modelo/CrudOfertaEmpleo.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.modelo;
+
+import com.conexion.Conexion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author carlos
+ */
+public class CrudOfertaEmpleo extends Conexion{
+     public void insertarOfertaEmpleo(OfertaEmpleo oe)throws Exception{
+        try {
+            this.conectar();
+            String sql="insert into ofertaEmpleo values(?,?,?,?,?,?,?,?,?,?,?)";
+            PreparedStatement pre=this.getCon().prepareStatement(sql);
+            pre.setInt(1, oe.getIdOfertaEmpleo());
+            pre.setInt(2, oe.getPuestoVacante());
+            pre.setString(3, oe.getTipoContratacion());
+            pre.setString(4, oe.getNivelExperiencia());
+            pre.setString(5, oe.getGenero());
+            pre.setInt(6, oe.getEdad());
+            pre.setFloat(7, oe.getSalario());
+            pre.setInt(8, oe.getIdEmpresa());
+            pre.setInt(9, oe.getIdPais());
+            pre.setInt(10, oe.getIdDepartamento());
+            pre.setString(11, oe.getDescripcionOferta());
+            pre.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        }
+        finally{
+            this.desconectar();
+        }
+    }
+    
+    public void modificarOfertaEmpleo(OfertaEmpleo oe)throws Exception{
+        try {
+            this.conectar();
+            String sql="update ofertaEmpleo set puestoVacaante=?, tipoContratacion=?, nivelExperiencia=?, genero=?, edad=?, salario=?, idEmpresa=?, idPais=?, idDepartamento=?, descripcionOferta=? where idOfertaEmpleo=?";
+            PreparedStatement pre=this.getCon().prepareStatement(sql);
+            pre.setInt(1, oe.getPuestoVacante());
+            pre.setString(2, oe.getTipoContratacion());
+            pre.setString(3, oe.getNivelExperiencia());
+            pre.setString(4, oe.getGenero());
+            pre.setInt(5, oe.getEdad());
+            pre.setFloat(6, oe.getSalario());
+            pre.setInt(7, oe.getIdEmpresa());
+            pre.setInt(8, oe.getIdPais());
+            pre.setInt(9, oe.getIdDepartamento());
+            pre.setString(10, oe.getDescripcionOferta());
+            pre.setInt(11, oe.getIdOfertaEmpleo());
+            pre.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        }
+        finally{
+            this.desconectar();
+        }
+    }
+    
+    public void eliminarOfertaEmpleo(OfertaEmpleo oe)throws Exception{
+        try {
+            this.conectar();
+            String sql="delete from ofertaEmpleo where idOfertaEmpleo=?";
+            PreparedStatement pre=this.getCon().prepareStatement(sql);
+            pre.setInt(1,oe.getIdOfertaEmpleo());
+            pre.executeUpdate();
+        } catch (Exception e) {
+            throw e;
+        }
+        finally{
+            this.desconectar();
+        }
+    }
+    
+    public List<OfertaEmpleo> mostrarOfertaEmpleo()throws Exception{
+        List<OfertaEmpleo> listaOfertaEmpleo=new ArrayList();
+        ResultSet res;
+        try {
+           this.conectar();
+            String sql="select*from ofertaEmpleo";
+            PreparedStatement pre =this.getCon().prepareCall(sql);
+            res=pre.executeQuery();
+            while(res.next()){
+                OfertaEmpleo oe=new OfertaEmpleo();
+                oe.setIdOfertaEmpleo(res.getInt("idOfertaEmpleo"));
+                oe.setPuestoVacante(res.getInt("puestoVacaante"));
+                oe.setTipoContratacion(res.getString("tipoContratacion"));
+                oe.setNivelExperiencia(res.getString("nivelExperiencia"));
+                oe.setGenero(res.getString("genero"));
+                oe.setEdad(res.getInt("edad"));
+                oe.setSalario(res.getFloat("salario"));
+                oe.setIdEmpresa(res.getInt("idEmpresa"));
+                oe.setIdPais(res.getInt("idPais"));
+                oe.setIdDepartamento(res.getInt("idDepartamento"));
+                oe.setDescripcionOferta(res.getString("descripcionOferta"));
+                listaOfertaEmpleo.add(oe);
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+        finally{
+            this.desconectar();
+        }
+        return listaOfertaEmpleo;
+    }
+}
